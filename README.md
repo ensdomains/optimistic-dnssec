415deeaca640e7821285be38a5af8f955c6e015a
decanus <dean.eigenmann@icloud.com>
2018-11-02 16:49:24 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# optimistic-dnssec
+# Optimistic DNSSEC
+
+[![Build Status](https://travis-ci.org/ensdomains/optimistic-dnssec.svg?branch=master)](https://travis-ci.org/ensdomains/dnssec-oracle) [![License](https://img.shields.io/badge/License-BSD--2--Clause-blue.svg)](LICENSE)
---
+++ contracts/Optimist.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.24;
+
+import "@ensdomains/dnssec-oracle/contracts/DNSSECImpl";
+
+contract Optimist {
+
+    /// @notice This function allows the user to submit a DNSSEC proof for a certain amount of ETH.
+    function commit() external payable {
+
+    }
+
+    /// @notice This function allows a user to challenge the validity of a DNSSEC proof submitted.
+    function challenge() external {
+
+    }
+
+}
