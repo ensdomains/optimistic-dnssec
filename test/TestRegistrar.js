4dbd35fadb5b1e197a1539ac05abc90e8e75e323
decanus <dean.eigenmann@icloud.com>
2018-12-16 16:13:50 +0700

    added some more unit tests

--- test/TestRegistrar.js
+++ test/TestRegistrar.js
@@ -37,7 +37,6 @@ contract('Registrar', function(accounts) {
         it('should succeed when submitting with a valid stake and name', async () => {
             await registrar.submit(dns.hexEncodeName('foo.test.'), '0x0', accounts[1], {value: stake});
 
-
             let record = await registrar.records.call(namehash.hash('foo.test'));
 
             assert.equal(record[0], accounts[0]);
@@ -46,4 +45,71 @@ contract('Registrar', function(accounts) {
 
     });
 
+    describe('challenge', async () => {
+
+        it('should fail to challenge when period has expired', async () => {
+            await registrar.submit(dns.hexEncodeName('foo.test.'), '0x0', accounts[1], {value: stake});
+
+            await utils.advanceTime(cooldown + 1);
+
+            try {
+                await registrar.challenge(dns.hexEncodeName('foo.test.'), '0x0');
+            } catch (error) {
+                return utils.ensureException(error);
+            }
+
+            assert.fail('did not fail');
+        });
+
+        it('should fail to challenge when proofs do not match', async () => {
+            await registrar.submit(dns.hexEncodeName('foo.test.'), '0x0', accounts[1], {value: stake});
+
+            try {
+                await registrar.challenge(dns.hexEncodeName('foo.test.'), '0x1');
+            } catch (error) {
+                return utils.ensureException(error);
+            }
+
+            assert.fail('did not fail');
+        });
+
+        // @todo should fail when proof is valid
+
+//        it('should successfully challenge when proof is invalid', async () => {
+//
+//            // @todo requires proper data
+//
+//            await registrar.submit(dns.hexEncodeName('foo.test.'), '0x0', accounts[1], {value: stake});
+//
+//            await registrar.challenge(dns.hexEncodeName('foo.test.'), '0x0');
+//        });
+    });
+
+    describe('commit', async () => {
+
+        it('should fail to commit when cooldown period has not expired', async () => {
+            await registrar.submit(dns.hexEncodeName('foo.test.'), '0x0', accounts[1], {value: stake});
+
+            try {
+                await registrar.commit(dns.hexEncodeName('foo.test.'));
+            } catch (error) {
+                return utils.ensureException(error);
+            }
+
+            assert.fail('did not fail');
+        });
+
+        // @todo should fail when challenged
+
+        it('should successfully commit when cooldown period has expired', async () => {
+            await registrar.submit(dns.hexEncodeName('foo.test.'), '0x0', accounts[1], {value: stake});
+
+            await utils.advanceTime(cooldown + 1);
+
+            await registrar.commit(dns.hexEncodeName('foo.test.'));
+
+            assert.equal(await ens.owner(namehash.hash('foo.test')), accounts[1]);
+        });
+
+    });
 });
